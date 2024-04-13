99646cd9f4034dea8245dec176a58079ef08989a
7binh <87206514+7binh@users.noreply.github.com>
2024-04-13 07:44:44 +0700

    First Commit

---
+++ src/components/headerApp/HeaderApp.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HeaderApp = () => {
+  return (
+    <div className="bg-white p-5">
+      <div className="w-32 mx-10">
+        <img
+          className="bg-cover w-full h-full"
+          src="https://theme.hstatic.net/200000420363/1000988446/14/logo.png?v=2262"
+          alt="logo tin hoc ngoi sao"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default HeaderApp;
